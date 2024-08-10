2d04c91c83d995ee602b509cbc7e57ddb33727e1
Shishamo <shishamodesu@outlook.jp>
2024-08-10 19:01:32 +0900

    a

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var oneang = PI / 180;
 
 var yamip = 0;
 var yps = 0;
+var yamisoul = 0;
 var value = [
     10,
     100,
@@ -46,7 +47,8 @@ var res = [
     "./res/img/nabe/05.png",
     "./res/img/nabe/06.png",
     "./res/img/nabe/07.png",
-    "./res/img/big_yaminabe.png"
+    "./res/img/big_yaminabe.png",
+    "./res/img/tensei.png"
 ];
 
 function range(x1, x2, y1, y2) {
@@ -61,6 +63,33 @@ function yps_update(){
     return tmp;
 }
 
+function tensei(){
+    value = [
+        10,
+        100,
+        1000,
+        10000,
+        100000,
+        1000000,
+        10000000,
+        100000000
+    ];
+    buy_num = [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ];
+    yamisoul += yamip / 10000;
+    yamip = 0;
+    yps = 0;
+    window.alert("a");
+}
+
 window.onload = function() {
     var game = new Game(1280, 720);
     game.preload(res);
@@ -68,7 +97,7 @@ window.onload = function() {
     game.onload = function() {
         var L_yamip = new Label();
         L_yamip.x = 0;
-        L_yamip.y = 0;
+        L_yamip.y = 25;
         L_yamip.color = "black";
         L_yamip.text = yamip + "闇鍋ポイント";
         L_yamip.on("enterframe", function() {
@@ -76,11 +105,11 @@ window.onload = function() {
         });
         var L_yps = new Label();
         L_yps.x = 0;
-        L_yps.y = 20;
+        L_yps.y = 45;
         L_yps.color = "black";
         L_yps.text = yps + "yps";
         L_yps.on("enterframe", function() {
-            L_yps.text = yps + "yps";
+            L_yps.text = yps + "yps + " + yamisoul + "%";
         });
         var L_text = new Array(8);
         for(var i = 0;i < L_text.length;i++) {
@@ -92,18 +121,26 @@ window.onload = function() {
                 L_text[i].text = value[i] + "闇鍋ポイント<br>所持数:" + buy_num[i] + "<br>yps:" + str[i];
             }
             L_text[i].x = 575;
-            L_text[i].y = 50 + i * 75;
+            L_text[i].y = 75 + i * 75;
             game.rootScene.addChild(L_text[i]);
         }
+        var L_tensei = new Label();
+        L_tensei.text = "闇鍋ソウル:" + yamisoul;
+        L_tensei.x = 0;
+        L_tensei.y = 70;
+        var S_tensei = new Sprite(50, 25);
+        S_tensei.image = game.assets[res[9]];
+        S_tensei.x = 0;
+        S_tensei.y = 0;
         var S_big = new Sprite(400, 400);
         S_big.image = game.assets[res[8]];
         S_big.x = 0;
-        S_big.y = 50;
+        S_big.y = 75;
         S_big.on("enterframe", function() {
             this.rotation = Math.sin(game.frame * oneang * 5) * 25;
             yps = yps_update();
             if(game.frame % game.fps == 0) {
-                yamip += yps;
+                yamip += yps * ((100 + yamisoul) / 100);
             }
             for(var i = 0;i < L_text.length;i++) {
                 if(i == 0) {
@@ -113,21 +150,22 @@ window.onload = function() {
                     L_text[i].text = value[i] + "闇鍋ポイント<br>所持数:" + buy_num[i] + "<br>yps:" + str[i];
                 }
             }
+            L_tensei.text = "闇鍋ソウル:" + yamisoul;
         });
         var S_nabe = new Array(8);
         for(var i = 0;i < S_nabe.length;i++) {
             S_nabe[i] = new Sprite(75, 75);
             S_nabe[i].image = game.assets[res[i]];
             S_nabe[i].x = 500;
-            S_nabe[i].y = 50 + i * 75;
+            S_nabe[i].y = 75 + i * 75;
             game.rootScene.addChild(S_nabe[i]);
         }
         game.rootScene.addEventListener("touchstart", function(e) {
-            if(range(200, e.localX, 250, e.localY) <= 200) {
-                yamip += buy_num[0] + 1;
+            if(range(200, e.localX, 275, e.localY) <= 200) {
+                yamip += buy_num[0] + 1 * ((100 + yamisoul) / 100);
             }
             for(var i = 0;i < buy_num.length;i++) {
-                if(range(537.5, e.localX, 87.5 + i * 75, e.localY) <= 37.5){
+                if(range(537.5, e.localX, 112.5 + i * 75, e.localY) <= 37.5){
                     if(yamip >= value[i]){
                         yamip -= value[i];
                         value[i] *= 1.1;
@@ -135,9 +173,14 @@ window.onload = function() {
                     }
                 }
             }
+            if(range(25, e.localX, 12.5, e.localY) <= 12.5) {
+                tensei();
+            }
         });
         game.rootScene.addChild(L_yamip);
         game.rootScene.addChild(L_yps);
+        game.rootScene.addChild(L_tensei);
+        game.rootScene.addChild(S_tensei);
         game.rootScene.addChild(S_big);
     }
     game.start();
